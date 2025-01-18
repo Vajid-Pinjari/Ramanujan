4ca8c80cb2bed5a11a61ed454411720896c6fc2b
Vajid-Pinjari <vajidmahmadhusen@mirafra.com>
2025-01-18 22:33:49 +0530

    probe function added in i2c_driver platform driver

--- I2c_driver/ramanujan_i2c.c
+++ I2c_driver/ramanujan_i2c.c
@@ -1,4 +1,5 @@
 /* Writing a I2C driver for Ramanujan SOC*/
+
 #include<linux/uacess.h>
 #include <linux/clk.h>
 #include <linux/clkdev.h>
@@ -13,21 +14,104 @@
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 
-#define I2C_CONTROL_REG 0x00
-#define I2C_STATUS_REG 0x04
-#define I2C_DLEN_REG 0x08
+#define I2C_CONTROL_REG    0x00
+#define I2C_STATUS_REG     0x04
+#define I2C_DLEN_REG       0x08
 #define I2C_SLAVE_ADDR_REG 0x0C
-#define I2C_DATA_FIFO_REG 0x10
-#define I2C_CLK_DEV_REG 0x14
+#define I2C_DATA_FIFO_REG  0x10
+#define I2C_CLK_DEV_REG    0x14
 #define I2C_DATA_DELAY_REG 0x18
-#define I2C_CLK_STRETCH 0x1C
+#define I2C_CLK_STRETCH    0x1C
 
-struct i2c_device{
-	void __iomem *base;
+//I2C control Register bits
+#define I2C_C_READ  BIT(0) //Read Bit
+#define I2C_C_CLEAR BIT(4) //BIT 4 & 5.
+#define I2C_C_ST    BIT(7) //Start Xfer.
+#define I2C_C_INTD  BIT(8) //Int on Done.
+#define I2C_C_INTT  BIT(9) //Int on Xfer.
+#define I2C_C_INTR  BIT(10) //Int on RX.
+#define I2C_C_EN    BIT(15)  //Enable I2C.
+
+//I2C Status Register
+#define I2C_S_TA	BIT(0) //Xfer Active.
+#define I2C_S_DONE	BIT(1) //Xfer Done.
+#define I2C_S_TXW	BIT(2) //FIFO needs writing.
+#define I2C_S_RXR	BIT(3) //FIFO needs Reading.
+#define I2C_S_TXD	BIT(4) //Accept Data.
+#define I2C_S_RXD	BIT(5) //Contains Data.
+#define I2C_S_TXE	BIT(6) //Empty.
+#define I2C_S_RXF	BIT(7) //Full.
+#define I2C_S_ERR	BIT(8) //ACK err.
+#define I2C_S_CLKT	BIT(9) //Clock Stretch Timeout.
+
+//I2C DEL(Delay) Register
+#define I2C_FEDL_SHIFT	16 //Falling Edge Delay.
+#define I2C_REDL_SHIFT	0  //Rising Edge Delay.
+
+//I2C CDIV Register
+/* Unreferenced 
+#define BCM2835_I2C_CDIV_MIN	0x0002
+#define BCM2835_I2C_CDIV_MAX	0xFFFE
+*/
+
+
+
+struct mfr_i2c_device{
+	struct device *dev;
+	void __iomem *regs;
 	int irq;
 	struct i2c_adapter adap;
-	struct completion xfer_done;
+	struct completion completion;
+	struct i2c_msg *curr_msg;
+	struct clk *bus_clk;
 };
+
+
+static int mfr_i2c_probe(struct platform_device *pdev)
+{
+	struct mfr_i2c_device *i2c_dev;
+	int ret;
+	struct i2c_adapter *adap;
+	struct clk *mclk;
+	u32 bus_clk_rate;
+
+	i2c_dev = devm_kzalloc(&pdev->dev,sizeof(*i2c_dev), GFP_KERNEL);
+	if(!i2c_dev)
+		return -ENOMEM;
+	platform_set_drvdata(pdev,i2c_dev);  //struct device
+	i2c_dev->dev = &pdev->dev;
+	init_completion(&i2c_dev->completion);
+
+	i2c_dev->regs = devm_platform_get_and_ioremap_resource(pdev, 0, NULL);
+	if(IS_ERR(i2c_dev->regs))
+		return PTR_ERR(i2c_dev->regs);
+
+	
+
+}
+
+
+static const struct of_device_id mfr_i2c_of_match[]={
+	{ 
+		.name = "mfr_i2c",
+		.type = "i2c",
+		.compatible = "mfr_i2c0",
+	},
+	{ .compatible = "mfr_i2c1" , .data = &mfr_i2c_quirks },
+	{},
+};
+MODULE_DEVICE_TABLE(of, mfr_i2c_of_match);
+
+static struct platform_driver i2c_platform_driver = {
+.probe = mfr_i2c_probe,
+.remove = mfr_i2c_remove,
+.driver = {
+	.name = "mfr_i2c",
+	.of_match_table = mfr_i2c_of_match,
+},
+};
+
+module_platform_driver(i2c_platform_driver);
 
 
 /*Driver Metadata*/
