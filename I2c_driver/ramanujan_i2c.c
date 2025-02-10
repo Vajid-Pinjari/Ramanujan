022284ed0bda2482d62343877a9daa74aa27f87d
Vajid-Pinjari <vajidmahmadhusen@mirafra.com>
2025-02-10 15:21:33 +0530

    probe-completed

--- I2c_driver/ramanujan_i2c.c
+++ I2c_driver/ramanujan_i2c.c
@@ -44,6 +44,7 @@
 #define I2C_S_RXF	BIT(7) //Full.
 #define I2C_S_ERR	BIT(8) //ACK err.
 #define I2C_S_CLKT	BIT(9) //Clock Stretch Timeout.
+#define I2C_S_LEN	BIT(10) /* Fake bit for SW error reporting */
 
 //I2C DEL(Delay) Register
 #define I2C_FEDL_SHIFT	16 //Falling Edge Delay.
@@ -182,6 +183,207 @@ clk_hw_register_clkdev(&prev->hw , "div",dev_name(dev));
     return devm_clk_register(dev, &prev->hw);
 }
 
+void mfr_drain_rxfifo(struct mfr_i2c_dev *i2c_dev)
+{
+    u32 val;
+    while(i2c_dev->msg_buf_remaining)
+    {
+        val = mfr_i2c_readl(i2c_dev, I2C_STATUS_REG);
+        if(!(val & I2C_S_RXD))
+        break;
+        *i2c_dev->msg_buf = mfr_i2c_readl(i2c_dev,I2C_DATA_FIFO_REG);
+        i2c_dev->msg_buf++;
+        i2c_dev->msg_buf_remaining--;
+    }
+}
+
+void mfr_fill_txfifo(struct mfr_i2c_dev *i2c_dev)
+{
+    u32 val;
+    while(i2c_dev->msg_buf_remaining)
+    {
+        val = mfr_i2c_readl(i2c_dev, I2C_STATUS_REG);
+        if(!(val & I2C_S_TXD))
+            break;
+        mfr_i2c_writel(i2c_dev , I2C_DATA_FIFO_REG, *i2c_dev->msg_buf);
+        i2c_dev->msg_buf++;
+        i2c_dev->msg_buf_remaining--;
+    }
+}
+
+static void mfr_i2c_start_transfer(struct mfr_i2c_dev * i2c_dev)
+{
+    u32 c = I2C_C_ST |  I2C_C_EN;
+    struct i2c_msg *msg = i2c_dev->curr_msg;
+    bool last_msg = (i2c_dev->num_msgs ==1 );
+
+    if(!i2c_dev->num_msgs)
+    return;
+
+    i2c_dev->num_msgs--;
+    i2c_dev->msg_buf = msg->buf;
+    i2c_dev->msg_buf_remaining = msg->len;
+
+    if(msg->flags & I2C_M_RD)
+    c |= I2C_C_READ | I2C_C_INTR;
+    else 
+    c |= I2C_C_INTT ;
+    if(last_msg)
+    c |= I2C_C_INTD;
+
+    mfr_i2c_writel(i2c_dev, I2C_SLAVE_ADDR_REG, msg->addr);
+    mfr_i2c_writel(i2c_dev , I2C_DLEN_REG , msg->len);
+    mfr_i2c_writel(i2c_dev, I2C_CONTROL_REG, c);
+
+}
+
+
+
+static void mfr_i2c_finish_transfer(struct mfr_i2c_dev *i2c_dev)
+{
+	i2c_dev->curr_msg = NULL;
+	i2c_dev->num_msgs = 0;
+
+	i2c_dev->msg_buf = NULL;
+	i2c_dev->msg_buf_remaining = 0;
+}
+
+static irqreturn_t mfr_i2c_isr(int this_irq, void *data)
+{ 
+    struct mfr_i2c_dev *i2c_dev = data;
+    u32 val , err;
+    
+    val = mfr_i2c_readl(i2c_dev,I2C_STATUS_REG);
+
+    err = val & (I2C_S_CLKT | I2C_S_ERR);
+    if(err)
+    {
+        i2c_dev->msg_err = err;
+        goto complete;
+    }
+
+    if(val & I2C_S_DONE )
+    {
+        if(!i2c_dev->curr_msg)
+        {
+            dev_err(i2c_dev->dev , "Got unexpected interrupt(from firmware?)\n");
+        }
+        else if(i2c_dev->curr_msg->flags & I2C_M_RD )
+        {
+            mfr_drain_rxfifo(i2c_dev);
+            val = mfr_i2c_readl(i2c_dev, I2C_STATUS_REG);
+        }
+
+        if((val & I2C_S_RXD) || i2c_dev->msg_buf_remaining)
+            i2c_dev->msg_err = I2C_S_LEN;
+        else 
+            i2c_dev->msg_err =0 ;
+        
+        goto complete;
+    }
+
+    if(val & I2C_S_TXW){
+        if(!i2c_dev->msg_buf_remaining){
+            i2c_dev->msg_err = val | I2C_S_LEN;
+            goto complete;
+        }
+
+        mfr_fill_txfifo(i2c_dev);
+
+        if(i2c_dev->num_msgs && !i2c_dev->msg_buf_remaining)
+        {
+            i2c_dev->curr_msg++;
+            mfr_i2c_start_transfer(i2c_dev);
+        }
+
+        return IRQ_HANDLED;
+    }
+
+    if(val & I2C_S_RXR )
+    {
+        if(!i2c_dev->msg_buf_remaining)
+        {
+            i2c_dev->msg_err = val | I2C_S_LEN ;
+            goto complete;
+        }
+        mfr_drain_rxfifo(i2c_dev);
+        return IRQ_HANDLED;
+    }
+    return IRQ_NONE;
+
+complete : mfr_i2c_writel(i2c_dev, I2C_CONTROL_REG,I2C_C_CLEAR);
+           mfr_i2c_writel(i2c_dev, I2C_STATUS_REG, I2C_S_CLKT |
+           I2C_S_ERR | I2C_S_DONE);
+           complete(&i2c_dev->completion);
+
+           return IRQ_HANDLED;
+}
+
+static int mfr_i2c_xfer(struct i2c_adapter *adap , struct i2c_msg msgs[],int num)
+{
+    struct mfr_i2c_dev *i2c_dev = i2c_get_adapdata(adap);
+    unsigned long time_left;
+    int i;
+
+    /* 1: Check if multiple read messages are requested 
+	         Only a single read message is supported and it must be the last */
+
+    for(i=0;i<(num-1);i++)
+    {
+        if(msgs[i].flags & I2C_M_RD)
+        {
+            dev_warn_once(i2c_dev->dev, "only one read message supported, has to be last\n");
+            return -EOPNOTSUPP;   // Return error if unsupported request is found
+        }
+    }
+
+    /* Step 2: Initialize transfer parameters */
+    i2c_dev->curr_msg = msgs;   // Store the message array
+    i2c_dev->num_msgs = num;    // Store the number of messages
+    reinit_completion(&i2c_dev->completion);  // Reinitialize the completion structure
+
+	/* Step 3: Start the I2C transfer */
+    mfr_i2c_start_transfer(i2c_dev);
+
+    /* Step 4: Wait for transfer completion with timeout */
+    time_left = wait_for_completion_timeout(&i2c_dev->completion,adap->timeout);
+    
+    /*Step 5: Finish transfer*/
+    mfr_i2c_finish_transfer(i2c_dev);
+
+    /* Step 6: Check if transfer timed out */
+    if(!time_left)
+    {
+        mfr_i2c_writel(i2c_dev ,I2C_CONTROL_REG, I2C_C_CLEAR);
+        return -ETIMEDOUT;
+    }
+
+    /* Step 7: Check for errors during transfer */
+    if(!i2c_dev->msg_err)
+        return num;
+    /* Log the error message if transfer failed */
+    dev_dbg(i2c_dev->dev, "i2c_transfer_failed : %x\n",i2c_dev->msg_err);
+
+    /* Step 8: Return appropriate error code based on failure type */
+    if(i2c_dev->msg_err & I2C_S_ERR)
+        return -EREMOTEIO;
+
+    return -EIO;
+}
+
+static u32 mfr_i2c_func(struct i2c_adapter *adap)
+{
+    return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL;
+}
+
+static const struct i2c_algorithm mfr_i2c_algo = {
+    .master_xfer = mfr_i2c_xfer,
+    .functionality = mfr_i2c_func,
+};
+
+static const struct i2c_adapter_quirks mfr_i2c_quirks = {
+    .flags = I2C_AQ_NO_CLK_STRETCH,
+};
 
 
 static int mfr_i2c_probe(struct platform_device *pdev)
@@ -242,24 +444,55 @@ static int mfr_i2c_probe(struct platform_device *pdev)
     }
 
     // Step 10: Retrieve the interrupt line for the device
+    i2c_dev->irq = platform_get_irq(pdev,0);
+    if(i2c_dev->irq < 0)
+    {
+        ret = i2c_dev->irq;
+        goto err_disable_unprepare_clk;
+    }
 	
     // Step 11: Request an IRQ and set the ISR for handling interrupts
+    ret = request_irq(i2c_dev->irq, mfr_i2c_isr, IRQF_SHARED, dev_name(&pdev->dev), i2c_dev);
+    if(ret)
+    {
+        dev_err(&pdev->dev, "Could not request IRQ\n");
+        goto err_disable_unprepare_clk;
+    }
 
     // Step 12: Initialize the I2C adapter structure
-	
+    adap = &i2c_dev->adapter;
     // Step 13: Set adapter metadata (name, owner, algorithm, parent, etc.)
-	
-    // Step 14: Register the adapter with the I2C subsystem
+    i2c_set_adapdata(adap,i2c_dev);
+    adap->owner = THIS_MODULE;
+    adap->class = I2C_CLASS_DEPRECATED;
+    snprintf(adap->name, sizeof(adap->name), "mfr (%s)", of_node_full_name(pdev->dev.of_node));
+    adap->algo = &mfr_i2c_algo;
+    adap->dev.parent = &pdev->dev;
 
+    // Step 14: Register the adapter with the I2C subsystem
+    adap->dev.of_node = pdev->dev.of_node;
+    adap->quirks = of_device_get_match_data(&pdev->dev);	
+    
     // Step 15: Configure hardware registers (disable clock stretching timeout)
-	
+    mfr_i2c_writel(i2c_dev , I2C_CLK_STRETCH, 0);
     // Step 16: Write default configurations to hardware registers
+    mfr_i2c_writel(i2c_dev , I2C_CONTROL_REG,0);
 
     // Step 17: Add the adapter to the I2C framework
+    ret = i2c_add_adapter(adap);
+    if(ret){
+        goto err_free_irq;
+    }
+
+    return 0;
 	
     // Error Handling: Free IRQ, disable clock, and cleanup on failure
+err_free_irq: 
+    free_irq(i2c_dev->irq,i2c_dev);
 err_disable_unprepare_clk:
 	clk_disable_unprepare(i2c_dev->bus_clk);
+err_put_exclusive_rate:
+    clk_rate_exclusive_put(i2c_dev->bus_clk);
 
     return ret;
 }
@@ -276,7 +509,7 @@ static const struct of_device_id mfr_i2c_of_match[]={
 		.type = "i2c",
 		.compatible = "mfr_i2c0",
 	},
-/*	{ .compatible = "mfr_i2c1" , .data = &mfr_i2c_quirks },*/
+	{ .compatible = "mfr_i2c1" , .data = &mfr_i2c_quirks },
 	{},
 };
 MODULE_DEVICE_TABLE(of, mfr_i2c_of_match);
@@ -296,12 +529,4 @@ module_platform_driver(i2c_platform_driver);
 /*Driver Metadata*/
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ramanujan SOC :Mirafra");
-MODULE_DESCRIPTION("I2C Driver for RAMANUJAN");
-
-/*
-In Falling edge delay shift register 
-we are deviding divider / 16
-fedl = divider/16 ;(2^n);
-& it should be greater than or equal to 1
-to take 16 as a scaling factor is there any specific parameter ?
-*/
+MODULE_DESCRIPTION("Platform :I2C Driver for RAMANUJAN");
--- I2c_driver/ramanujan_i2c.mod.c
+++ I2c_driver/ramanujan_i2c.mod.c
@@ -35,14 +35,6 @@ MODULE_INFO(retpoline, "Y");
 
 static const char ____versions[]
 __used __section("__versions") =
-	"\x30\x00\x00\x00\xe6\x87\x67\x7a"
-	"devm_platform_get_and_ioremap_resource\0\0"
-	"\x18\x00\x00\x00\x2c\xfa\xc5\xe3"
-	"devm_clk_get\0\0\0\0"
-	"\x14\x00\x00\x00\x6e\x4a\x6e\x65"
-	"snprintf\0\0\0\0"
-	"\x18\x00\x00\x00\xce\xd8\x69\xc5"
-	"__clk_get_name\0\0"
 	"\x20\x00\x00\x00\xd4\xd5\xa8\xc4"
 	"clk_hw_register_clkdev\0\0"
 	"\x1c\x00\x00\x00\x13\x4a\x1e\x8e"
@@ -59,14 +51,32 @@ __used __section("__versions") =
 	"clk_disable\0"
 	"\x18\x00\x00\x00\x0a\xe7\x77\xb0"
 	"clk_unprepare\0\0\0"
+	"\x20\x00\x00\x00\x8c\xd8\xb4\xac"
+	"clk_rate_exclusive_put\0\0"
 	"\x14\x00\x00\x00\xa6\x88\x55\x81"
 	"clk_enable\0\0"
+	"\x1c\x00\x00\x00\x0c\xaf\xed\x68"
+	"platform_get_irq\0\0\0\0"
+	"\x20\x00\x00\x00\x8e\x83\xd5\x92"
+	"request_threaded_irq\0\0\0\0"
+	"\x18\x00\x00\x00\x87\x9c\x0e\xbd"
+	"i2c_add_adapter\0"
+	"\x14\x00\x00\x00\x3b\x4a\x51\xc1"
+	"free_irq\0\0\0\0"
 	"\x18\x00\x00\x00\x8d\xd0\xef\xb9"
 	"dev_err_probe\0\0\0"
 	"\x1c\x00\x00\x00\xcb\xf6\xfd\xf0"
 	"__stack_chk_fail\0\0\0\0"
 	"\x24\x00\x00\x00\xda\x90\x30\x27"
 	"platform_driver_unregister\0\0"
+	"\x24\x00\x00\x00\x0e\xd7\x3a\x4a"
+	"wait_for_completion_timeout\0"
+	"\x1c\x00\x00\x00\x46\x5f\x4c\xa1"
+	"__dynamic_dev_dbg\0\0\0"
+	"\x2c\x00\x00\x00\xc6\xfa\xb1\x54"
+	"__ubsan_handle_load_invalid_value\0\0\0"
+	"\x14\x00\x00\x00\x2f\x7a\x25\xa6"
+	"complete\0\0\0\0"
 	"\x14\x00\x00\x00\xbb\x6d\xfb\xbd"
 	"__fentry__\0\0"
 	"\x1c\x00\x00\x00\xca\x39\x82\x5b"
@@ -77,6 +87,14 @@ __used __section("__versions") =
 	"devm_kmalloc\0\0\0\0"
 	"\x20\x00\x00\x00\xb5\x41\x87\x60"
 	"__init_swait_queue_head\0"
+	"\x30\x00\x00\x00\xe6\x87\x67\x7a"
+	"devm_platform_get_and_ioremap_resource\0\0"
+	"\x18\x00\x00\x00\x2c\xfa\xc5\xe3"
+	"devm_clk_get\0\0\0\0"
+	"\x14\x00\x00\x00\x6e\x4a\x6e\x65"
+	"snprintf\0\0\0\0"
+	"\x18\x00\x00\x00\xce\xd8\x69\xc5"
+	"__clk_get_name\0\0"
 	"\x18\x00\x00\x00\xd7\xd3\x75\x6d"
 	"module_layout\0\0\0"
 	"\x00\x00\x00\x00\x00\x00\x00\x00";
@@ -85,5 +103,7 @@ MODULE_INFO(depends, "");
 
 MODULE_ALIAS("of:Nmfr_i2cTi2c*Cmfr_i2c0");
 MODULE_ALIAS("of:Nmfr_i2cTi2c*Cmfr_i2c0C*");
+MODULE_ALIAS("of:N*T*Cmfr_i2c1");
+MODULE_ALIAS("of:N*T*Cmfr_i2c1C*");
 
-MODULE_INFO(srcversion, "064AAB80C8A6C74D47819C5");
+MODULE_INFO(srcversion, "68D2ED96366B192519893CB");
